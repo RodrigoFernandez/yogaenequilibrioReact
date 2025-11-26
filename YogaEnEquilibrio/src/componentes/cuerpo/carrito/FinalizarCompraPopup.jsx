b40ecf599a31808b23fdb0b7e1bd6dbf08d38bf0
Rodrigo Fernandez <rdr.rkh+github2@gmail.com>
2025-11-25 23:11:36 -0300

    Se agregó la lógica para el popup de finalización de la compra. Falta la lógica para terminar la compra si el usuario está logueado, sino se debe pedir el login.

---
+++ YogaEnEquilibrio/src/componentes/cuerpo/carrito/FinalizarCompraPopup.jsx
@@ -0,0 +1,67 @@
+import {Portal} from '../../../Portal';
+import { useCarrito } from "../../../contextos/CarritoContext";
+import style from './FinalizarCompraPopup.module.css';
+
+const ItemCompra = ({item}) => {
+    return (
+        <tr>
+            <td>{item.producto.nombre}</td>
+            <td>(x{item.cantidad})</td>
+            <td></td>
+            <td className={style['total-producto']}>${item.cantidad * item.producto.precio}</td>
+        </tr>
+    );
+};
+
+const TablaItemsCompra = ({carrito, getTotalCarrito}) => {
+    return (
+        <table className={style['tabla-items-compra']}>
+            <tbody>
+                {carrito.map( (item) => <ItemCompra key={item.producto.id} item={item}></ItemCompra>)}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td className={style['resaltado']} colSpan={3}>Total:</td>
+                    <td className={`${style.resaltado} ${style['total-producto']}`}>$ {getTotalCarrito()}</td>
+                </tr>
+            </tfoot>
+        </table>
+    );
+};
+
+const ListadoFinalizarCompra = ({cerrarDialogo, carrito, getTotalCarrito}) => {
+    return (
+        <div className={style['modal-finalizar-compra-contenido']}>
+            <header className={style['modal-finalizar-compra-header']}>
+                <h3>Finalizar Compra</h3>
+            </header>
+            <div className={style['modal-finalizar-compra-body']}>
+                <p>Detalle de la compra:</p>
+                <TablaItemsCompra carrito={carrito} getTotalCarrito={getTotalCarrito}></TablaItemsCompra>
+            </div>
+                    
+            <div className={style['modal-botonera']}>
+                <button className="boton" >Confirmar compra</button>
+                <button className="boton" onClick={cerrarDialogo}>Cancelar</button>
+            </div>
+        </div>
+    );
+}
+
+const FinalizarCompraPopup = ({esDialogoAbierto, cerrarDialogo}) => { 
+    const { carrito, getTotalCarrito } = useCarrito();
+
+    return (
+        <>
+            {esDialogoAbierto && 
+            <Portal>
+                <div className={style['modal-overlay']}></div>
+                <div className={style['modal-finalizar-compra']} open={esDialogoAbierto} aria-modal="true">
+                    <ListadoFinalizarCompra cerrarDialogo={cerrarDialogo} carrito={carrito} getTotalCarrito={getTotalCarrito}></ListadoFinalizarCompra>
+                </div>
+            </Portal>}
+        </>
+    );
+};
+
+export default FinalizarCompraPopup;
--- YogaEnEquilibrio/src/componentes/cuerpo/carrito/TotalCarrito.jsx
+++ YogaEnEquilibrio/src/componentes/cuerpo/carrito/TotalCarrito.jsx
@@ -1,8 +1,21 @@
+import {useState} from 'react';
 import { useCarrito } from "../../../contextos/CarritoContext";
+import FinalizarCompraPopup from "./FinalizarCompraPopup";
 
 export const TotalCarrito = () => {
     const { carrito, getTotalCarrito } = useCarrito();
+    const [esFinalizarPopupAbierto, setEsFinalizarPopupAbierto] = useState(false);
     
+    const finalizarCompra = (e) => {
+        // Lógica para finalizar la compra
+        console.log("Acá debería salir el formulario para terminar la compra.");
+        setEsFinalizarPopupAbierto(true);
+    };
+
+    const cerrarDialogo = () => {
+        setEsFinalizarPopupAbierto(false);
+    };
+
     const getEstiloBoton = () => {
         return getTotalCarrito() === 0 ? "boton boton-deshabilitado" : "boton";
     }
@@ -17,7 +30,8 @@ export const TotalCarrito = () => {
                     <p>${getTotalCarrito()}</p>
                 </div>
             </div>
-            <button type="button" className={getEstiloBoton()} id="finalizarCompraBtn" disabled={getTotalCarrito() === 0}>Finalizar compra</button>
+            <button type="button" className={getEstiloBoton()} id="finalizarCompraBtn" disabled={getTotalCarrito() === 0} onClick={finalizarCompra} >Finalizar compra</button>
+            <FinalizarCompraPopup esDialogoAbierto={esFinalizarPopupAbierto} cerrarDialogo={cerrarDialogo}></FinalizarCompraPopup>
         </div>
     )
 };
